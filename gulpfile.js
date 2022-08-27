0a534651df84d25334c08dc2139d32ab9a574d7d
JEHM94 <jesus.e.hamel@gmail.com>
2022-08-27 16:09:52 -0400

    Images Optimization

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,74 @@
 // }
 
 // exports.myTask = myTask;
-const {src, dest, watch} = require("gulp");
+
+// CSS
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+// Images
+const webp = require('gulp-webp');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const avif = require('gulp-avif');
 
-function compileCss(done){
+// Functions CSS *********
+function compileCss(done) {
     src("src/scss/**/*.scss") // Identify the SASS file
-    .pipe(plumber())
-    .pipe(sass()) // Compile it
-    .pipe(dest("build/css")) // Save it
+        .pipe(plumber())
+        .pipe(sass()) // Compile it
+        .pipe(dest("build/css")) // Save it
 
     done(); // Tells gulp that the Task has reached the end of the function
 }
 
-function watchScssChanges(done){
+function watchScssChanges(done) {
     watch("src/scss/**/*.scss", compileCss); // Watch for changes on app.scss then run compileCss
 
     done();
 }
 
+// Functions Images *********
+function convertToWebp(done) {
+    const options = {
+        quality: 50,
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function convertToAvif(done) {
+    const options = {
+        quality: 50,
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function reduceImageSize(done) {
+    const options = {
+        optimizationLevel: 3,
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(options)))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+
+// Exports *********
+// Individuals
 exports.compileCss = compileCss;
 exports.watchScssChanges = watchScssChanges;
+exports.convertToWebp = convertToWebp;
+exports.convertToAvif = convertToAvif;
+exports.reduceImageSize = reduceImageSize;
+// Dev / Build
+exports.build = parallel(reduceImageSize, convertToWebp, convertToAvif, watchScssChanges);
