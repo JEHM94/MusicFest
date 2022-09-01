3aa8af42faa7d6612d169b7975a2aef358bddd6d
JEHM94 <jesus.e.hamel@gmail.com>
2022-09-01 00:07:18 -0400

    Post css & js

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,28 @@
 const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+// POST CSS
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 // Images
 const webp = require('gulp-webp');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const avif = require('gulp-avif');
+// POST Javascript
+const terser = require('gulp-terser-js');
 
 // Functions CSS *********
 function compileCss(done) {
     src("src/scss/**/*.scss") // Identify the SASS file
-        .pipe(plumber())
-        .pipe(sass()) // Compile it
-        .pipe(dest("build/css")) // Save it
+        .pipe(sourcemaps.init()) // Start css identification through sourcemaps
+        .pipe(plumber()) // Prevents workflow from stopping
+        .pipe(sass()) // Compile sass
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.')) // Write the sourcemaps on '.' the same folder
+        .pipe(dest("build/css")) // Save css into build
 
     done(); // Tells gulp that the Task has reached the end of the function
 }
@@ -64,6 +74,9 @@ function reduceImageSize(done) {
 // Functions JS *********
 function compileJS(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init()) // Start css identification through sourcemaps
+        .pipe(terser()) // Post JS
+        .pipe(sourcemaps.write('.')) // Write the sourcemaps on '.' the same folder
         .pipe(dest('build/js'));
 
     done();
